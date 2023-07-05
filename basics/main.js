1952c7632f4d94fb7a960d5224f2473a8bd938c3
Raktim Parasar <raktimparasar96@gmail.com>
2023-07-05 17:46:35 +0530

    basics

---
+++ basics/main.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var digit = 10;
+console.log(digit);
+// variable declaration
+var sum;
+var result;
+/**
+ * let declaration can be reinitialized, but
+ * const declarations much be initialized with value.
+ */
---
+++ basics/main.ts
@@ -0,0 +1,13 @@
+export {};
+let digit = 10;
+console.log(digit);
+
+// variable declaration
+
+let sum: number;
+const result: string;
+
+/**
+ * let declaration can be reinitialized, but
+ * const declarations much be initialized with value.
+ */
